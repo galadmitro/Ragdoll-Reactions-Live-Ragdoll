81dd822b7fa388a29be28b6be30a806bcd5120e3
galadmitro <galadmitro87392824@gmail.com>
2026-08-18 17:08:40 +0300

    updating...

--- src/main/java/com/yourname/activeragdoll/ActiveRagdollAddon.java
+++ src/main/java/com/yourname/activeragdoll/ActiveRagdollAddon.java
@@ -6,7 +6,6 @@ import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.phys.Vec3;
 
 import java.lang.reflect.Method;
 
@@ -14,7 +13,6 @@ import java.lang.reflect.Method;
 public class ActiveRagdollAddon {
     public static final String MODID = "activeragdoll";
     public static boolean isCollapsed = false;
-    private static Object activeRagdollInstance = null;
 
     public ActiveRagdollAddon(IEventBus modEventBus) {
         NeoForge.EVENT_BUS.register(this);
@@ -24,61 +22,27 @@ public class ActiveRagdollAddon {
     public void onPlayerTick(PlayerTickEvent.Post event) {
         if (event.getEntity() instanceof Player player) {
             if (player.level().isClientSide()) {
-                updateContinuousActiveRagdoll(player);
+                updateRagdollState(player);
             }
         }
     }
 
-    private void updateContinuousActiveRagdoll(Player player) {
+    private void updateRagdollState(Player player) {
         try {
-            if (activeRagdollInstance == null) {
-                Class<?> managerClass = Class.forName("dev.leo.ragdollreactions.ragdoll.RagdollManager");
-                Method getOrCreate = managerClass.getMethod("getOrCreateRagdoll", Player.class);
-                activeRagdollInstance = getOrCreate.invoke(null, player);
-            }
-
-            if (activeRagdollInstance != null) {
-                Method setKinematic = activeRagdollInstance.getClass().getMethod("setKinematic", boolean.class);
-                Method setSpringTarget = activeRagdollInstance.getClass().getMethod("setSpringTargetPose", Player.class);
-                Method setSpringStrength = activeRagdollInstance.getClass().getMethod("setSpringStrength", float.class);
-
-                if (isCollapsed) {
-                    setKinematic.invoke(activeRagdollInstance, false);
-                    setSpringStrength.invoke(activeRagdollInstance, 0.0f);
-                } else {
-                    setKinematic.invoke(activeRagdollInstance, false);
-                    setSpringStrength.invoke(activeRagdollInstance, 0.85f);
-                    setSpringTarget.invoke(activeRagdollInstance, player);
-                }
-            }
-        } catch (Exception e) {
-            fallbackSpringPhysics(player);
-        }
-    }
-
-    private void fallbackSpringPhysics(Player player) {
-        try {
-            Class<?> launcherClass = Class.forName("dev.leo.ragdollreactions.physics.ReactionLauncher");
-            if (isCollapsed) {
-                Method launchMethod = launcherClass.getMethod("launchReaction", Player.class, Vec3.class, float.class);
-                launchMethod.invoke(null, player, player.getDeltaMovement(), 1.0f);
-            } else {
-                Method updateMethod = launcherClass.getMethod("keepAlive", Player.class, float.class);
-                updateMethod.invoke(null, player, 0.85f);
+            Class<?> ragdollClass = Class.forName("dev.leo.ragdollreactions.ragdoll.Ragdoll");
+            Method getMethod = ragdollClass.getMethod("get", Player.class);
+            Object ragdollInstance = getMethod.invoke(null, player);
+            
+            if (ragdollInstance != null) {
+                Method setPhysics = ragdollInstance.getClass().getMethod("setPhysicsEnabled", boolean.class);
+                setPhysics.invoke(ragdollInstance, true);
+                
+                Method setActive = ragdollInstance.getClass().getMethod("setActiveRagdoll", boolean.class);
+                setActive.invoke(ragdollInstance, !isCollapsed);
             }
         } catch (Exception ignored) {
-            applyProceduralRagdollJoints(player);
-        }
-    }
-
-    private void applyProceduralRagdollJoints(Player player) {
-        if (!isCollapsed) {
-            Vec3 vel = player.getDeltaMovement();
-            double speed = Math.sqrt(vel.x * vel.x + vel.z * vel.z);
-            float wobble = (float) Math.sin(player.tickCount * 0.5f) * (float) speed * 0.15f;
-
             player.yBodyRotO = player.yBodyRot;
-            player.yBodyRot = player.getYRot() + wobble * 12.0f;
+            player.yBodyRot = player.getYRot();
             player.yHeadRotO = player.yHeadRot;
             player.yHeadRot = player.getYRot();
         }
